3992b5d77b6cc82f35410ce947bb0887e18b5cb4
Ryo Okamoto <ryo@aquahill.net>
2013-05-29 12:54:39 +0900

    initial import.

---
+++ lib/Test/Crontab/Format.pm
@@ -0,0 +1,39 @@
+package Test::Crontab::Format;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::Crontab::Format - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::Crontab::Format;
+
+=head1 DESCRIPTION
+
+Test::Crontab::Format is ...
+
+=head1 LICENSE
+
+Copyright (C) Ryo Okamoto.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ryo Okamoto E<lt>ryo@aquahill.netE<gt>
+
+=cut
+
